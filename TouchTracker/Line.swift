d84cecc9c530d9690656732ca40ac0c65a69fbc7
FIRAT KARAKUYU <firatkarakuyu@firats-mac.local>
2020-03-12 20:08:58 -0700

    Initial Commit

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,16 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Fırat Karakuyu on 5.03.2020.
+//  Copyright © 2020 FiratiOS. All rights reserved.
+//
+
+import Foundation
+
+import CoreGraphics
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
+
